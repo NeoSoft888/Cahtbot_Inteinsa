c4bfb05084fbeb36b214162054af52befd063cb1
NeoSoft888 <admin@inteinsa.com>
2025-05-29 11:43:15 -0500

    ChatBot_Inteinsa

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Servidor Flask en Render funcionando'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.form
+    print("Datos recibidos:", data)
+    return 'OK', 200
+
+if __name__ == '__main__':
+    app.run()
